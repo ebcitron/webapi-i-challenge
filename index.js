845037b701e76e8949ade49aa49fb648d7fc4400
Eli Citron <ebcitron@gmail.com>
2019-05-06 17:45:31 -0500

    Working

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+const express = require ('express');
+const db = require('./data/db');
+const server = express();
+server.use(express.json());
+
+const PORT = process.env.port || 5000;
+
+server.listen(PORT, () => { 
+    console.log(`\n*** Server Listening on ${PORT}  *** \n`)
+})
+
+server.get("/", (req,res)=> {
+    res.send("Hello World!!!")
+})
