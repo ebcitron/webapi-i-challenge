b24e7e3c51a085a3f07afb302d3f5e911b063c12
Eli Citron <ebcitron@gmail.com>
2019-05-06 18:20:51 -0500

    Still working

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const db = require('./data/db');
 const server = express();
 server.use(express.json());
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.port || 6666;
 
 
 
@@ -17,10 +17,38 @@ server.get('/', (req, res) => {
     })
 
 //POST TO CREATE USER
-
+server.post('/users', (req, res) => {
+    const userInfo = req.body;
+    console.log('Request Body Name', userInfo)
+db.users
+.insert(userInfo)
+.then(user => {
+    res.status(201).json({success: true, user});
+})
+.catch(({code, message}) => {
+    res.status(code).json({success: false, message });
+});
+});
 
 //GET THE USER ARRAY
 
+server.get('/users', (req,res) => {
+    db.users
+    .find()
+    .then(users => {
+        if(users){
+        res.status(200).json(users)
+        } else {
+            res.status(500).json({error: "The users information could not be retrieved"})
+        }
+    
+    })
+    .catch(({code, message}) => {
+        res.status(code).json({ message})
+    })
+})
+
+//GET THE DATE
 server.get('/date', (req,res) => {
     const now = new Date().toISOString();
     res.send(now);
